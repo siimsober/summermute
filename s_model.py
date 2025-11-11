f2859d5911255836e270300358dba144d45043ac
Siim Sõber <siims6ber@gmail.com>
2025-11-12 00:16:31 +0200

    real single head attention

--- s_model.py
+++ s_model.py
@@ -10,12 +10,14 @@ class myNN(nn.Module):
         self.embed_size = 64
         self.max_len = 256
 
+        # token + positional embeddings
         self.embed = nn.Embedding(vocab_size, self.embed_size)
         self.pos_embed = nn.Embedding(self.max_len, self.embed_size)
 
-        # trainable weights for fusing previous tokens
-        self.fuse = nn.Parameter(torch.randn(self.max_len, self.max_len))
-        self.value = nn.Linear(self.embed_size, self.embed_size)  # learned V
+        # attention projections
+        self.key = nn.Linear(self.embed_size, self.embed_size, bias=False)
+        self.query = nn.Linear(self.embed_size, self.embed_size, bias=False)
+        self.value = nn.Linear(self.embed_size, self.embed_size, bias=False)
 
         self.fc = nn.Linear(self.embed_size, vocab_size)
 
@@ -26,19 +28,23 @@ class myNN(nn.Module):
         tok_emb = self.embed(x)                      # [B, T, E]
         pos = torch.arange(T, device=x.device)
         pos_emb = self.pos_embed(pos)[None, :, :]    # [1, T, E]
-        x = tok_emb + pos_emb                          # [B, T, E]
+        x = tok_emb + pos_emb                        # [B, T, E]
 
+        # compute Q, K, V
+        Q = self.query(x)                            # [B, T, E]
+        K = self.key(x)                              # [B, T, E]
+        V = self.value(x)                            # [B, T, E]
+
+        # scaled dot-product attention
+        att = (Q @ K.transpose(-2, -1)) / (self.embed_size ** 0.5)  # [B, T, T]
+        
         # causal mask to only fuse preceding tokens
         mask = torch.tril(torch.ones(T, T, device=x.device))
-        fuse_weights = self.fuse[:T, :T]            # [T, T]
-        fuse_weights = fuse_weights.masked_fill(mask == 0, float('-inf'))
-        fuse_weights = F.softmax(fuse_weights, dim=-1)  # [T, T]
+        att = att.masked_fill(mask == 0, float('-inf'))
 
-        V = self.value(x)
+        att = F.softmax(att, dim=-1)                 # [B, T, T]
+        context = att @ V                            # [B, T, E]
 
-        # fuse embeddings
-        context = fuse_weights @ V                    # [B, T, E]
-
-        logits = self.fc(context)                     # [B, T, vocab_size]
+        logits = self.fc(context)                    # [B, T, vocab_size]
         return logits
     
